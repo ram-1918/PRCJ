8ed44993ea4e18ad62e4e7c06c47f22785baef0a
ram-1918 <109194855+ram-1918@users.noreply.github.com>
2022-10-19 20:28:19 -0400

    Add files via upload

---
+++ models.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+from django.db import models
+
+class item_models(models.Model):    
+    #devicename=models.CharField(max_length=100)
+    item_id = models.CharField(primary_key=True,max_length=100)
+    item_type=models.CharField(max_length=100)
+    item_image = models.ImageField(upload_to="allmodels")
+    item_weight = models.IntegerField()
+    item_status= models.BooleanField(default=False)
+
+
+class wishlist(models.Model):
+    devicename=models.CharField(max_length=100)
+    item_id=models.CharField(max_length=100)
+    item_type=models.CharField(max_length=100)
+    item_status= models.BooleanField(default=False)
+
+
+
+
